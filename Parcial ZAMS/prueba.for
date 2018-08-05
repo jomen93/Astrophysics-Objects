fdd1d06a2e524f9de87d93d2065b04c028ea889f
Johan Méndez <jsmendezh@unal.edu.co>
2018-08-05 04:18:32 -0500

    Primera versión del código de objetos astrofísicos

---
+++ Parcial ZAMS/prueba.for
@@ -0,0 +1,8 @@
+      program hola
+c	  Asi se comenta en fortran
+      integer :: i
+      write(*,*)'Hola, mundo'
+      read(*,*) i
+      write(*,*)'Su numero al cuadrado es', i*i
+      stop
+      end program
